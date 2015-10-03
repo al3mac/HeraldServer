f680b6e647e6ece3049e1086ee93b4c5987e9903
wojciechrauner <wojciech.rauner@gmail.com>
2015-10-03 03:25:52 +0200

    Small changes

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,7 @@
 
 var crypto = require('crypto');
 var shasum = crypto.createHash('sha1');
+var q = require('q');
 
 module.exports.controller = function(server, db) {
   server.route({
@@ -15,61 +16,4 @@ module.exports.controller = function(server, db) {
     path: '/api/user/{id}',
     handler: getUser
   });
-
-  function saveUser(request, reply) {
-    if(request.payload) {
-      shasum.update(request.payload);
-      var id = shasum.digest('base64');
-      db.get(id, function(err, user) {
-        if(user) {
-          reply({
-            status: 'FAILED',
-            description: 'User exists'
-          });
-        }
-      });
-      db.put(id, request.payload, function(err) {
-        if(err) {
-          console.log(err);
-          reply({
-            status: 'FAILED',
-            description: 'Failed to save user'
-          });
-        }
-        return;
-      });
-      reply({
-        status: 'OK',
-        description: 'Saved user',
-        userId: id
-      });
-    } else {
-      reply({
-        status: 'FAILED',
-        description: 'Empty request'
-      });
-    }
-  }
-
-  function getUser(request, reply) {
-    if(request.params.id) {
-      db.get(request.params.id, function(err, user) {
-        if(err) {
-          reply({
-            status: 'FAILED',
-            description: 'No user found'
-          });
-        }
-        if(user) {
-          reply(user);
-        }
-        return;
-      });
-    } else {
-      reply({
-        status: 'FAILED',
-        description: 'Empty request'
-      });
-    }
-  }
 };
